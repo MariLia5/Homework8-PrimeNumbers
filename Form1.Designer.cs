239843389da3d9846147c74a8afb19f2f71c2d14
0 <0>
2025-05-23 12:20:37 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PrimeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNumber1 = new System.Windows.Forms.TextBox();
+            this.tbNumber2 = new System.Windows.Forms.TextBox();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.lbStart = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNumber1
+            // 
+            this.tbNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbNumber1.Location = new System.Drawing.Point(40, 118);
+            this.tbNumber1.Name = "tbNumber1";
+            this.tbNumber1.Size = new System.Drawing.Size(116, 41);
+            this.tbNumber1.TabIndex = 0;
+            this.tbNumber1.TextChanged += new System.EventHandler(this.tbNumber1_TextChanged);
+            // 
+            // tbNumber2
+            // 
+            this.tbNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbNumber2.Location = new System.Drawing.Point(324, 118);
+            this.tbNumber2.Name = "tbNumber2";
+            this.tbNumber2.Size = new System.Drawing.Size(116, 41);
+            this.tbNumber2.TabIndex = 1;
+            this.tbNumber2.TextChanged += new System.EventHandler(this.tbNumber1_TextChanged);
+            // 
+            // lbResult
+            // 
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbResult.Location = new System.Drawing.Point(34, 297);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(400, 170);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.Text = "Результат:";
+
+            // 
+            // lbStart
+            // 
+            this.lbStart.AutoSize = true;
+            this.lbStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbStart.Location = new System.Drawing.Point(96, 60);
+            this.lbStart.Name = "lbStart";
+            this.lbStart.Size = new System.Drawing.Size(300, 36);
+            this.lbStart.TabIndex = 3;
+            this.lbStart.Text = "Введите два числа:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(65, 202);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(347, 50);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Получить простые числа";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 492);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lbStart);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.tbNumber2);
+            this.Controls.Add(this.tbNumber1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumber1;
+        private System.Windows.Forms.TextBox tbNumber2;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Label lbStart;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void tbNumber2_TextChanged(object sender, EventArgs e){ }
+        private void tbNumber1_TextChanged(object sender, EventArgs e){ }
+
+        // Проверка на простое число
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        // Получение списка чисел
+        private List<int> GetPrimesInRange(int start, int end)
+        {
+            List<int> primes = new List<int>();
+
+            // Проверка, что start меньше end
+            if (start > end)
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                    primes.Add(i);
+            }
+            return primes;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(tbNumber1.Text);
+                int num2 = int.Parse(tbNumber2.Text);
+
+                List<int> primes = new List<int>();
+                int start = Math.Min(num1, num2);
+                int end = Math.Max(num1, num2);
+
+                for (int i = start; i <= end; i++)
+                {
+                    if (IsPrime(i)) primes.Add(i);
+                }
+
+                if (primes.Count == 0)
+                {
+                    lbResult.Text = "Простых чисел в диапазоне не найдено.";
+                }
+                else
+                {
+                    string result = $"Найдено {primes.Count} простых чисел:\n";
+                    for (int i = 0; i < primes.Count; i++)
+                    {
+                        result += primes[i] + " ";
+                        if ((i + 1) % 5 == 0) result += "\n";
+                    }
+                    lbResult.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                lbResult.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+    }
+}
